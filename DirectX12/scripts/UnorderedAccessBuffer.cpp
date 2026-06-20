f8a30748290657723abfbee2eba041ddf434300f
hibiki <h_ka196@icloud.com>
2026-06-20 17:17:20 +0900

    UAバッファークラスの実装

--- DirectX12/scripts/UnorderedAccessBuffer.cpp
+++ DirectX12/scripts/UnorderedAccessBuffer.cpp
@@ -6,12 +6,35 @@ UnorderedAccessBuffer::UnorderedAccessBuffer(Graphic& graphic, int sizeOfElement
 	:mSizeOfElement(sizeOfElement)
 	, mNumElement(numElement)
 {
-	auto device = graphic.getDevice();
 	//バッファーの詳細設定
+	auto desc = getResourceDesc();
+	//ヒープ設定
+	auto prop = getHeapProperties();
+
+	//バッファーの作成とマッピング
+	auto device = graphic.getDevice();
+	createAndMapBuffers(*device, desc, prop);
+}
+
+const ID3D12Resource& UnorderedAccessBuffer::getBufferOnGPU() const
+{
+	return *mBuffersOnGPU[Graphic::FrameCount].Get();
+}
+
+const void* UnorderedAccessBuffer::getBufferOnCPU() const
+{
+	return mBuffersOnCPU[Graphic::FrameCount];
+}
+
+D3D12_RESOURCE_DESC UnorderedAccessBuffer::getResourceDesc()
+{
 	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(mSizeOfElement * mNumElement);
 	desc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;	//アンオーダーアクセスに設定
-	int bufferNo = 0;
+	return desc;
+}
 
+D3D12_HEAP_PROPERTIES UnorderedAccessBuffer::getHeapProperties()
+{
 	D3D12_HEAP_PROPERTIES prop{};
 	prop.Type = D3D12_HEAP_TYPE_CUSTOM;		//ヒープ設定を手動で行う
 	prop.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;	//CPUがキャッシュへ書き込む際、ライトバック方式をとるよう設定
@@ -19,11 +42,17 @@ UnorderedAccessBuffer::UnorderedAccessBuffer(Graphic& graphic, int sizeOfElement
 	prop.CreationNodeMask = 1;									//デフォルトのプライマリGPUでメモリを確保
 	prop.VisibleNodeMask = 1;									//デフォルトのプライマリGPUからメモリが見えるようにする
 
+	return prop;
+}
+
+void UnorderedAccessBuffer::createAndMapBuffers(ID3D12Device& device, D3D12_RESOURCE_DESC& desc, D3D12_HEAP_PROPERTIES& prop)
+{
 	mBuffersOnGPU.resize(Graphic::FrameCount);
 	mBuffersOnCPU.resize(Graphic::FrameCount);
+	int bufferNo = 0;
 	//バッファーの確保
 	for (auto& buffer : mBuffersOnGPU) {
-		device->CreateCommittedResource(
+		device.CreateCommittedResource(
 			&prop,
 			D3D12_HEAP_FLAG_NONE,
 			&desc,
@@ -38,10 +67,5 @@ UnorderedAccessBuffer::UnorderedAccessBuffer(Graphic& graphic, int sizeOfElement
 
 		bufferNo++;
 	}
-	
 }
 
-void UnorderedAccessBuffer::init()
-{
-
-}
--- DirectX12/scripts/UnorderedAccessBuffer.h
+++ DirectX12/scripts/UnorderedAccessBuffer.h
@@ -5,8 +5,14 @@ public:
 	//要素のサイズと数を入力
 	UnorderedAccessBuffer(class Graphic& graphic, int sizeOfElement, int numElement);
 
+	const ID3D12Resource& getBufferOnGPU() const;
+	const void* getBufferOnCPU() const;
+
 private:
-	void init();
+	D3D12_RESOURCE_DESC getResourceDesc();
+	D3D12_HEAP_PROPERTIES getHeapProperties();
+	void createAndMapBuffers(ID3D12Device& device, D3D12_RESOURCE_DESC& desc, D3D12_HEAP_PROPERTIES& prop);
+
 	const int mSizeOfElement;
 	const int mNumElement;
 	std::vector<ComPtr<ID3D12Resource>> mBuffersOnGPU;
